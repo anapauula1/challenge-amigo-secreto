377ad397bbda0f3d87f7de2a489bc76e0b883cd2
anapauula1 <anapaularodriguez101@gmail.com>
2025-02-27 14:02:04 -0300

    creacion de lista

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// creo una variable para guardar los nombres de los amigos
+// como son muchos nos conviene hacer una lista
+
+let listaAmigos = []; // sera un arreglo (una lista)
+
