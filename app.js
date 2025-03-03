a3e8f7545a2161d99bb63deb47b226efb39004b1
anapauula1 <anapaularodriguez101@gmail.com>
2025-03-03 15:10:32 -0300

    funciones y nuevos elementos

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 let listaAmigos = []; 
 // agarro el elemento que quiero guardar
 let inputListaAmigos = document.getElementById("amigos");
+// llamo a un objeto que pobre utilizar en las proximas lineas
+// selecciono al elemento
+let listaDeAmigos = [] // lista vacia
+const ulAmigos = document.getElementById("listaAmigos");
+function agregarAmigo(){
+    // funcion de onclick
+    listaAmigos.push(inputListaAmigos.value);
+    // le estamos diciendo que empuje el resultado
+    // el nombre que mostramos en el input
+}
+
+
+
+
 
 
 
